e6b377da2a33c96c9a8654fecf57cc202f87b764
germanp007 <pintogerman281@gmail.com>
2024-02-07 12:17:35 -0300

    sdgghfas

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import IconMinus from "./components/icons/IconMinus";
 import { data } from "./constant";
 // import BackgroundDesktop from "./components/background/BackgroundDesktop";
 // import BackgroundMobile from "./components/background/BackgroundMobile";
-// import mobile from "/src/images/background-pattern-mobile.svg";
-// import desktop from "/src/images/background-pattern-desktop.svg";
+import mobile from "../public/images/background-pattern-mobile.svg";
 
 interface ClickState {
   button1: boolean;
@@ -54,16 +53,19 @@ const App = () => {
       <main className="bg-LightPink flex flex-col justify-center w-screen">
         <section
           style={{
-            background: `url(../src/images/background-pattern-mobile.svg) center/cover no-repeat`,
+            // background: `url(${
+            //   widthScreen > 600 ? desktop : mobile
+            // }) center/cover no-repeat`,
             height: "232px",
             width: "100%",
-
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             flexDirection: "column",
           }}
-        ></section>
+        >
+          {mobile}
+        </section>
         <section className="relative w-screen h-[150vh] md:h-[100vh] flex justify-center">
           <article className="text-DarkPurple w-[330px] p-6 absolute top-[-20%] sm:top-[-25%] bg-White rounded-lg flex flex-col gap-5 sm:w-[600px]">
             <div className="flex justify-start mt-4">
@@ -121,6 +123,7 @@ const App = () => {
             <strong>German Pinto.</strong>
           </a>
         </h6>
+        <img src={mobile} alt="mobile" />
       </footer>
     </>
   );
